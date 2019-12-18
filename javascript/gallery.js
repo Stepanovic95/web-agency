79602ab5a22bc183d6d7de0c7a872f9a7c77546a
Marko Stepanovic <57499711+Stepanovic95@users.noreply.github.com>
2019-12-18 21:10:22 +0100

    Added photo gallery
    problems with nav bar on gallery

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,4 +1,4 @@
-
-$(function () {
-    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-    });
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
